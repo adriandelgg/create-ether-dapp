f3eaba9610de8b189c55f1fb8075080a7e91c5e0
Adrian Delgado <adriandelgadoc1@gmail.com>
2021-12-12 20:25:53 -0800

    Added backend

--- backend/index.ts
+++ backend/index.ts
@@ -7,7 +7,9 @@ import helmet from "helmet";
 const app = express();
 
 mongoose
-	.connect("")
+	.connect(
+		"mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+	)
 	.then(() => console.log("Connected to MongoDB..."))
 	.catch(err => console.error("FAILED to connect to MongoDB: " + err));
 

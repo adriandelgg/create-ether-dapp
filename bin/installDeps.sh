e70ee029ee42391fa0c2634e70a71770381f3eed
Adrian Delgado <adriandelgadoc1@gmail.com>
2022-01-19 00:39:22 -0800

    Fixed script issues

--- bin/installDeps.sh
+++ bin/installDeps.sh
@@ -2,4 +2,5 @@
 [ -d backend ] && cd backend && yarn install && cd ..
 [ -d frontend ] && cd frontend && yarn install && cd ..
 [ -d hardhat ] && cd hardhat && yarn install && cd ..
-find . -type f -name .example.env -execdir mv {} .env ";"
+# find . -type f -name .example.env -execdir mv {} .env ";"
+find . -type f -name .env -execdir mv {} .example.env ";"

6313e1dc931d8172831dd4149072b14dae2f4600
Adrian Delgado <adriandelgadoc1@gmail.com>
2022-01-19 11:26:32 -0800

    Added license

--- bin/installDeps.sh
+++ bin/installDeps.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# Used to install all dependencies
+# Also checks to make sure the folders are present in case they get delted.
 [ -d backend ] && cd backend && yarn install && cd ..
 [ -d frontend ] && cd frontend && yarn install && cd ..
 [ -d hardhat ] && cd hardhat && yarn install && cd ..
-# find . -type f -name .example.env -execdir mv {} .env ";"
-find . -type f -name .env -execdir mv {} .example.env ";"
+
+# Renames .example.env files to .env
+find . -type f -name .example.env -execdir mv {} .env ";"

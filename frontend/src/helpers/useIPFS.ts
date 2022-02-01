195bd17e14b1ba3b8cfd5f2254e88ecd4c09547a
Adrian Delgado <adriandelgadoc1@gmail.com>
2022-02-01 15:11:22 -0800

    Minor updates

--- frontend/src/helpers/useIPFS.ts
+++ frontend/src/helpers/useIPFS.ts
@@ -1,7 +1,7 @@
-import { create } from "ipfs-http-client";
-import axios from "axios";
+import { create } from 'ipfs-http-client';
+import axios from 'axios';
 
-const ipfs = create({ host: "ipfs.infura.io", port: 5001, protocol: "https" });
+const ipfs = create({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 
 /**
  * Uploads the file to IPFS.
@@ -25,14 +25,14 @@ export async function uploadIPFS(fileBuffer: Buffer) {
  */
 export async function pinNFT(hash: string) {
 	try {
-		const url = "https://api.pinata.cloud/pinning/pinByHash";
+		const url = 'https://api.pinata.cloud/pinning/pinByHash';
 		await axios.post(
 			url,
 			{ hashToPin: hash },
 			{
 				headers: {
-					pinata_api_key: process.env.NEXT_PUBLIC_PINATA_API_KEY,
-					pinata_secret_api_key: process.env.NEXT_PUBLIC_PINATA_SECRET_KEY
+					pinata_api_key: process.env.NEXT_PUBLIC_PINATA_API_KEY!,
+					pinata_secret_api_key: process.env.NEXT_PUBLIC_PINATA_SECRET_KEY!
 				}
 			}
 		);

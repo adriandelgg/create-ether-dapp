dc9d8b5a08e67a8225e9ba320d63f5519f040d0c
Adrian Delgado <adriandelgadoc1@gmail.com>
2022-02-11 20:16:52 -0800

    Removed DaisyUI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,31 +1,20 @@
 module.exports = {
-	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
-	darkMode: "class", // or 'media' or 'class'
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+	darkMode: 'class', // or 'media' or 'class'
 	theme: {
 		extend: {
 			fontSize: {
-				xxs: "0.55rem"
-			},
-			colors: {
-				green: {
-					main: "#009C00",
-					bp1: "#009900",
-					bp2: "#99cc00",
-					bp3: "#088308"
-				}
+				xxs: '0.55rem'
 			},
 			borderRadius: {
-				"4xl": "2em",
-				"5xl": "3em",
-				"6xl": "4em"
+				'4xl': '2em',
+				'5xl': '3em',
+				'6xl': '4em'
 			},
 			padding: {
-				11.5: "2.97em"
+				11.5: '2.97em'
 			}
 		}
 	},
-	plugins: [require("daisyui"), require("@tailwindcss/typography")],
-	daisyui: {
-		base: false
-	}
+	plugins: [require('@tailwindcss/typography')]
 };
